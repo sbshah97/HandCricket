5ba16b593d2907b0f16937fe3f77bd32ba238557
Salman Shah <sbs.191197@gmail.com>
2016-05-03 11:14:21 +0530

    Update toss.js

--- toss.js
+++ toss.js
@@ -37,4 +37,34 @@ console.log("Computer: " + computerChoice);
 
 var tossChoice = userChoice + computerChoice;
 
-console.log(userChoice + computerChoice);
+console.log(tossChoice);
+
+if (tossChoice % 2 == 0 && userChoice == "even")
+{
+    var usertossChoice = prompt("Do you want to Bat or Bowl?")  
+    }
+
+else if (tossChoice % 2 == 1 && userChoice == "odd")
+{
+    var usertossChoice = prompt("Do you want to bat or bowl?")  
+    }
+
+else 
+{
+    
+    tossChoice1 = Math.random();
+    if(tossChoice1 <= 0.50)
+    {
+    var usertossChoice = "bat";
+        }
+        
+    else 
+    {
+        var usertosssChoice = "bowl"
+        }
+    
+    }
+
+console.log("You have to" + userTosschoice + "first");
+
+
