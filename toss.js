80abcbb22e49faea7829d0d3e85aa0825ba8ff16
Salman Shah <sbs.191197@gmail.com>
2016-05-14 20:37:04 +0530

    Update toss.js

--- toss.js
+++ toss.js
@@ -2,38 +2,8 @@ console.log("Hello, my name is Salman Shah the creator of this online version of
 
 var userChoice1 = prompt ("Do you choose Odd OR Even?");
 
-var userChoice = prompt("Choose  Your Number");
-var computerChoice = Math.random();
-
-if (computerChoice <= 0.17) 
-{
-	computerChoice = 1;
-} 
-
-else if(computerChoice <= 0.34 && computerChoice >= 0.18) 
-{
-	computerChoice = 2;
-} 
-
-else if(computerChoice <= 0.50 && computerChoice > 0.34)
-{
-	computerChoice = 3;
-} 
-
-else if(computerChoice <= 0.67 && computerChoice > 0.50)
-{
-	computerChoice = 4;
-} 
-
-else if(computerChoice <= 0.84 && computerChoice > 0.67)
-{
-	computerChoice = 5;
-} 
-
-else
-{
-	computerChoice = 6;
-} 
+var userChoice = prompt("Choose Your Number");
+var computerChoice = Math.floor(Math.random()*6 + 1);
 
 console.log("Computer: " + computerChoice);
 
